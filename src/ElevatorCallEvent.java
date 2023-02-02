08cf732922a7e0964da3f21006f125801366975f
Osamudiamen Nwoko <carlosnwoko123@gmail.com>
2023-02-01 22:12:41 -0500

    Created classes

---
+++ src/ElevatorCallEvent.java
@@ -0,0 +1,33 @@
+import java.time.LocalTime;
+
+public class ElevatorCallEvent {
+
+    private final LocalTime time;
+    private final int floorNumber;
+    private final Direction direction;
+    private final int elevatorNumber;
+    public enum Direction {UP, DOWN}
+
+    public ElevatorCallEvent(LocalTime time, int floorNumber, Direction direction, int elevatorNumber) {
+        this.time = time;
+        this.floorNumber = floorNumber;
+        this.direction = direction;
+        this.elevatorNumber = elevatorNumber;
+    }
+
+    public LocalTime getTime() {
+        return time;
+    }
+
+    public int getFloorNumber() {
+        return floorNumber;
+    }
+
+    public Direction getDirection() {
+        return direction;
+    }
+
+    public int getElevatorNumber() {
+        return elevatorNumber;
+    }
+}
