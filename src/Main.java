5cf8a00cf60ec77924988f9e718e48582d30c020
Osamudiamen Nwoko <carlosnwoko123@gmail.com>
2023-02-04 23:49:41 -0500

    Updates

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,14 @@
+import java.time.LocalTime;
+
 public class Main {
 
     public static void main(String[] args) {
+        Scheduler scheduler = new Scheduler();
+        Floor floor = new Floor(1, scheduler);
+        Elevator elevator = new Elevator(5, scheduler);
+        ElevatorCallEvent event = new ElevatorCallEvent(LocalTime.now(), 2, ElevatorCallEvent.Direction.UP, 3);
 
+        floor.start();
+        elevator.start();
     }
 }
