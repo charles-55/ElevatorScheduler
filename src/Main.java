37c285198af2457a0bb9c77d4512b28eb0a58a5b
Osamudiamen Nwoko <carlosnwoko123@gmail.com>
2023-02-04 23:50:02 -0500

    Updates

--- src/Main.java
+++ src/Main.java
@@ -6,7 +6,6 @@ public class Main {
         Scheduler scheduler = new Scheduler();
         Floor floor = new Floor(1, scheduler);
         Elevator elevator = new Elevator(5, scheduler);
-        ElevatorCallEvent event = new ElevatorCallEvent(LocalTime.now(), 2, ElevatorCallEvent.Direction.UP, 3);
 
         floor.start();
         elevator.start();
