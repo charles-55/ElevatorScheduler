fd67d21bf9ea0e8abd7275bd17f761ddb8ab02d7
Osamudiamen Nwoko <carlosnwoko123@gmail.com>
2023-03-11 21:46:44 -0500

    Nick's Updates

--- src/Scheduler.java
+++ src/Scheduler.java
@@ -18,6 +18,7 @@ public class Scheduler extends Thread {
     private DatagramPacket floorSendPacket, floorReceivePacket, elevatorSendPacket, elevatorReceivePacket;
     private InetAddress floorAddress, elevatorAddress;
     private DatagramSocket floorReceivingSocket, floorSendingSocket, elevatorReceivingSocket, elevatorSendingSocket;
+    private States state;
     private static final int FLOOR_RECEIVING_PORT = 2000, FLOOR_SENDING_PORT = 2300, ELEVATOR_SENDING_PORT = 2100, ELEVATOR_RECEIVING_PORT = 2200;
 
     /**
@@ -25,6 +26,7 @@ public class Scheduler extends Thread {
      */
     public Scheduler() {
         try {
+            state = States.IDLE;
             floorReceivingSocket = new DatagramSocket(FLOOR_RECEIVING_PORT);
             floorSendingSocket = new DatagramSocket();
             elevatorReceivingSocket = new DatagramSocket(ELEVATOR_RECEIVING_PORT);
@@ -47,6 +49,7 @@ public class Scheduler extends Thread {
         try {
             System.out.println("SCHEDULER: Waiting for Packet from Floor...\n");
             floorReceivingSocket.receive(floorReceivePacket);
+            state = States.RECEIVING_TASK;
         } catch (IOException e) {
             System.out.println("SCHEDULER Error: Floor Socket Timed Out.");
             e.printStackTrace();
@@ -58,8 +61,10 @@ public class Scheduler extends Thread {
         elevatorSendPacket = new DatagramPacket(data, data.length, elevatorAddress, ELEVATOR_SENDING_PORT);
 
         try {
+
             System.out.println("SCHEDULER: Sending Packet to elevator: " + Arrays.toString(data) + "\n");
             elevatorSendingSocket.send(elevatorSendPacket);
+            state = States.SENDING_TASK;
         } catch (IOException e) {
             System.out.println("SCHEDULER Error: Elevator Socket Timed Out.");
             e.printStackTrace();
@@ -67,7 +72,7 @@ public class Scheduler extends Thread {
         }
 
         System.out.println("SCHEDULER: Packet sent to elevator.\n");
-
+        state = States.IDLE;
         try {
             Thread.sleep(50);
         } catch (InterruptedException e ) {
@@ -86,6 +91,7 @@ public class Scheduler extends Thread {
         try {
             System.out.println("SCHEDULER: Waiting for Packet from Elevator...\n");
             elevatorReceivingSocket.receive(elevatorReceivePacket);
+            state = States.RECEIVING_MESSAGE;
         } catch (IOException e) {
             System.out.println("SCHEDULER Error: Elevator Socket Timed Out.");
             e.printStackTrace();
@@ -99,6 +105,7 @@ public class Scheduler extends Thread {
         try {
             System.out.println("SCHEDULER: Sending Packet to Floor: " + Arrays.toString(data)+".\n");
             floorSendingSocket.send(floorSendPacket);
+            state = States.SENDING_MESSAGE;
         } catch (IOException e) {
             System.out.println("SCHEDULER Error: Floor Socket Timed Out.");
             e.printStackTrace();
@@ -106,12 +113,33 @@ public class Scheduler extends Thread {
         }
 
         System.out.println("SCHEDULER: Packet sent to floor.\n");
-
+        state = States.IDLE;
         try {
             Thread.sleep(50);
         } catch (InterruptedException e ) {
             e.printStackTrace();
             System.exit(1);
+        }
+    }
+
+    /**
+     * Prints the state of movement of the elevator and the state of its doors.
+     */
+    public void printAnalyzedState() {
+        System.out.print("SCHEDULER: ");
+
+        if(state.equals(States.IDLE)) {
+            System.out.println("The scheduler is idle.\n");
+        } else if(state.equals(States.RECEIVING_TASK)) {
+            System.out.println("The scheduler is receiving a task.\n");
+        } else if(state.equals(States.SENDING_TASK)) {
+            System.out.println("The scheduler is sending a task.\n");
+        } else if(state.equals(States.RECEIVING_MESSAGE)) {
+            System.out.println("The scheduler is receiving a message.\n");
+        } else if(state.equals(States.SENDING_MESSAGE)) {
+            System.out.println("The scheduler is sending a message.\n");
+        } else {
+            throw new RuntimeException("Error: The scheduler is in an unknown state.\n");
         }
     }
 
