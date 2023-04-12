5511320a0e630211aa9a1c3485c3006c8ed8c096
leslie <leslieejeh@cmail.com>
2023-04-11 20:57:12 -0400

    handlestate

--- src/Frame.java
+++ src/Frame.java
@@ -1,6 +1,14 @@
 import javax.swing.*;
 
 public class Frame  extends JFrame implements View{
+    private final ElevatorPanel
+
+
+    public Frame(){
+        super("Elevator");
+        this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+
+    }
 
 
 
